684971ca19ce046bc54f75c808c8bd6eb126df40
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-19 17:48:44 +0400

    Add brand products route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Frontend\ProductController;
 Route::get('/',[MainController::class,'index'])->name('home');
 Route::get('/internal-credit',[MainController::class,'credit'])->name('internal-credit');
 Route::get('/brands',[BrandsController::class,'index'])->name('brands');
+Route::get('/brand/{brand}',[MainController::class,'brand'])->name('brand.products');
 
 Route::middleware('guest')->group(function(){
     Route::get('/login',[AuthController::class,'login'])->name('front.login');
