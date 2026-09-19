e123d7f51beb260fc1c3911ac8ba686dcbf13c85
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-19 17:12:23 +0400

    Load product detail relations

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -3,20 +3,29 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use App\Models\Banners;
-use App\Models\Faq;
 use App\Models\Product\Product;
 use App\Services\SeoUrl;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function product($slug)
     {
-        $product = Product::findOrFail(SeoUrl::decodeSlug($slug));
+        $product = Product::with([
+            'brand',
+            'type',
+            'images',
+            'genders',
+            'ingredients',
+            'variants' => function ($query) {
+                $query
+                    ->where('active', 1)
+                    ->orderBy('price');
+            },
+            'variants.size',
+        ])->findOrFail(SeoUrl::decodeSlug($slug));
 
-        return view('frontend.product',[
-            'product'   => $product
+        return view('frontend.product', [
+            'product' => $product,
         ]);
     }
 }
