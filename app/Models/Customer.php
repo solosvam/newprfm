d384aeb012e43ca82f89ce9e71a05ecc25e7f390
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-20 16:56:45 +0400

    Add customer checkout relations

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -33,4 +33,6 @@ class Customer extends Authenticatable
             'active' => 'boolean',
         ];
     }
+    public function addresses(){ return $this->hasMany(CustomerAddress::class); }
+    public function orders(){ return $this->hasMany(Order::class); }
 }
