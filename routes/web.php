3ae874b7ee7b6a7aaa15d12a419b0b7b615bdd71
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-20 15:34:24 +0400

    Use SEO brand URLs

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Frontend\ProductController;
 Route::get('/',[MainController::class,'index'])->name('home');
 Route::get('/internal-credit',[MainController::class,'credit'])->name('internal-credit');
 Route::get('/brands',[BrandsController::class,'index'])->name('brands');
-Route::get('/brand/{brand}',[MainController::class,'brand'])->name('brand.products');
+Route::get('/brand/{slug}',[MainController::class,'brand'])->name('brand.products');
 
 Route::middleware('guest')->group(function(){
     Route::get('/login',[AuthController::class,'login'])->name('front.login');
