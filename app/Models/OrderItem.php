90c4a873ba19f8df3914578d4236d3bbee3e7b65
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-20 16:56:43 +0400

    Add checkout order models

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class OrderItem extends Model {protected $guarded=[];public function order(){return $this->belongsTo(Order::class);}}
