7c25d3577c141a6268a496d2462859e8e4463b98
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-21 14:04:08 +0400

    Show real favorites count in cabinet

--- resources/views/frontend/partials/cabinet-sidebar.blade.php
+++ resources/views/frontend/partials/cabinet-sidebar.blade.php
@@ -10,7 +10,7 @@
             <li><a href="{{ route('profile.personal') }}" class="{{ request()->routeIs('profile.personal*') ? 'cabinet-active' : '' }}">Şəxsi məlumatlar</a></li>
             <li><a href="{{ route('profile.orders') }}" class="{{ request()->routeIs('profile.orders') ? 'cabinet-active' : '' }}">Sifarişlərimin tarixçəsi</a></li>
             <li><a href="{{ route('profile.bonuses') }}" class="{{ request()->routeIs('profile.bonuses') ? 'cabinet-active' : '' }}">Bonus tarixçəsi</a></li>
-            <li><a href="{{ route('profile.wishlist') }}" class="{{ request()->routeIs('profile.wishlist') ? 'cabinet-active' : '' }}">Bəyəndiyim ətirlər <span>0</span></a></li>
+            <li><a href="{{ route('profile.wishlist') }}" class="{{ request()->routeIs('profile.wishlist') ? 'cabinet-active' : '' }}">Bəyəndiyim ətirlər <span>{{ auth()->user()->favoriteProducts()->count() }}</span></a></li>
             <li><a href="{{ route('profile.reviews') }}" class="{{ request()->routeIs('profile.reviews') ? 'cabinet-active' : '' }}">Rəylərim</a></li>
         </ul>
         <form method="POST" action="{{ route('front.logout') }}" class="cabinet-logout-form">@csrf
