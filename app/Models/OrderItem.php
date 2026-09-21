03d153ad848b49d6fccbcef1af2791f2e54a3b2f
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-21 11:32:49 +0400

    Add order item product relations

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -1,4 +1,15 @@
 <?php
 namespace App\Models;
+
+use App\Models\Product\Product;
+use App\Models\Product\ProductVariant;
 use Illuminate\Database\Eloquent\Model;
-class OrderItem extends Model {protected $guarded=[];public function order(){return $this->belongsTo(Order::class);}}
+
+class OrderItem extends Model
+{
+    protected $guarded = [];
+
+    public function order(){ return $this->belongsTo(Order::class); }
+    public function product(){ return $this->belongsTo(Product::class); }
+    public function variant(){ return $this->belongsTo(ProductVariant::class, 'product_variant_id'); }
+}
