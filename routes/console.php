3718cf8b3204906bcea8a56e2f93cc41de253153
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-21 12:25:28 +0400

    Explicitly register Parfumshop import command

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\ImportOldParfumshopCategory;
+
+Artisan::starting(function ($artisan) {
+    $artisan->resolveCommands([
+        ImportOldParfumshopCategory::class,
+    ]);
+});
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
