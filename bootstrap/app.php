0ff0170f5cc6078aed6021135cf16cdb1037cbd2
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-21 12:23:29 +0400

    Register application console commands

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withCommands([
+        __DIR__.'/../app/Console/Commands',
+    ])
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(setLangMiddleware::class);
         $middleware->redirectGuestsTo(function (Request $request) {
