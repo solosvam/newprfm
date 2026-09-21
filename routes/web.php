a10d96bc80556a4208cef71a47c968e5d4232178
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-21 14:10:54 +0400

    Add public guest wishlist route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/brands',[BrandsController::class,'index'])->name('brands');
 Route::get('/brand/{slug}',[BrandsController::class,'products'])->name('brand.products');
 Route::view('/cart', 'frontend.cart')->name('cart');
 Route::get('/cart/products',[ProductController::class,'cartProducts'])->name('cart.products');
+Route::get('/wishlist',[FavoriteController::class,'guest'])->name('wishlist');
 
 Route::middleware('guest')->group(function(){
     Route::get('/login',[AuthController::class,'login'])->name('front.login');
