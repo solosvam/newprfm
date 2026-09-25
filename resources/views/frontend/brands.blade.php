a6d4c38e0071905a4db9598642f63e37bbd2f2ad
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 03:56:39 +0400

    Use semantic translation keys across customer and catalog views

--- resources/views/frontend/brands.blade.php
+++ resources/views/frontend/brands.blade.php
@@ -7,7 +7,7 @@
                 <li>
                     <a href="#" onclick="window.history.back()">
                         <img src="{{asset('frontend/images/arrow-left.svg')}}" alt="" />
-                        <span>{{ __('Geri qayıt') }}</span>
+                        <span>{{ __('cart_go_back') }}</span>
                     </a>
                 </li>
                 <li>
@@ -21,7 +21,7 @@
                             <img src="{{asset('frontend/images/arrow-right.svg')}}" class="arrow" alt="" />
                         </li>
                         <li>
-                            <a href="{{route('brands')}}">{{ __('Markalar') }}</a>
+                            <a href="{{route('brands')}}">{{ __('reviews_brands') }}</a>
                         </li>
                     </ul>
                 </li>
