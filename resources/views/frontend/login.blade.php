20b8285c370a03d8c263a56c81a23be26b5af5a9
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 03:56:09 +0400

    Use semantic translation keys in storefront views

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -4,20 +4,20 @@
     <div class="login-container">
         <div class="login-page" id="loginBox">
             <div class="login-page__wrap">
-                <h1>{{ __('Şəxsi kabinet') }}</h1>
+                <h1>{{ __('auth_my_account') }}</h1>
                 <form class="login-page__wrap__form" id="customerLoginForm">
                     @csrf
                     <input type="tel" id="loginMobile" placeholder="994 __ ___ __ __" autocomplete="tel" inputmode="numeric" />
                     <div id="passwordArea" class="hide-form">
-                        <input type="password" id="loginPassword" placeholder="{{ __('Şifrə') }}" autocomplete="current-password" />
+                        <input type="password" id="loginPassword" placeholder="{{ __('auth_password') }}" autocomplete="current-password" />
                     </div>
                     <p class="error-message hide-form" id="loginError"></p>
                     <div class="actions hide-form" id="passwordActions">
-                        <button type="submit" id="loginSubmitBtn">{{ __('Daxil ol') }}</button>
+                        <button type="submit" id="loginSubmitBtn">{{ __('auth_sign_in') }}</button>
                     </div>
                 </form>
                 <div class="actions login-register-link">
-                    <button type="button" id="registerBtn">{{ __('Qeydiyyat') }}</button>
+                    <button type="button" id="registerBtn">{{ __('auth_register') }}</button>
                 </div>
             </div>
         </div>
@@ -25,18 +25,18 @@
         <div id="otpSection" class="hide-form">
             <div class="otpSection-wrapper">
                 <div class="headers">
-                    <h1>{{ __('Hesabı təsdiqlə') }}</h1>
+                    <h1>{{ __('auth_verify_account') }}</h1>
                     <span class="otp-sent-message" id="otpMessage"></span>
-                    <input type="text" id="otpInput" inputmode="numeric" maxlength="6" placeholder="{{ __('OTP kodu') }}" />
+                    <input type="text" id="otpInput" inputmode="numeric" maxlength="6" placeholder="{{ __('auth_otp_code') }}" />
                     <p class="error-message hide-form" id="otpError"></p>
                 </div>
                 <div class="otp-section-form-label">
                     <span class="seconds" id="otpTimer">01:00</span>
-                    <button type="button" class="send-again-text" id="resendOtp" disabled>{{ __('Yenidən göndər') }}</button>
+                    <button type="button" class="send-again-text" id="resendOtp" disabled>{{ __('auth_resend_code') }}</button>
                 </div>
                 <div class="actions">
-                    <button type="button" id="backBtn">{{ __('Geriyə') }}</button>
-                    <button type="button" id="otpSubmitBtn">{{ __('Təsdiqlə') }}</button>
+                    <button type="button" id="backBtn">{{ __('auth_back') }}</button>
+                    <button type="button" id="otpSubmitBtn">{{ __('auth_confirm') }}</button>
                 </div>
             </div>
         </div>
@@ -44,13 +44,13 @@
         <div id="setPasswordSection" class="hide-form">
             <div class="otpSection-wrapper">
                 <div class="headers">
-                    <h1>{{ __('Şifrə təyin et') }}</h1>
-                    <input type="password" id="newPassword" placeholder="{{ __('Yeni şifrə') }}" autocomplete="new-password" />
-                    <input type="password" id="newPasswordConfirmation" placeholder="{{ __('Şifrənin təkrarı') }}" autocomplete="new-password" />
+                    <h1>{{ __('auth_set_password') }}</h1>
+                    <input type="password" id="newPassword" placeholder="{{ __('auth_new_password') }}" autocomplete="new-password" />
+                    <input type="password" id="newPasswordConfirmation" placeholder="{{ __('auth_confirm_password') }}" autocomplete="new-password" />
                     <p class="error-message hide-form" id="passwordError"></p>
                 </div>
                 <div class="actions">
-                    <button type="button" id="setPasswordBtn">{{ __('Şifrəni yadda saxla') }}</button>
+                    <button type="button" id="setPasswordBtn">{{ __('auth_save_password') }}</button>
                 </div>
             </div>
         </div>
@@ -67,10 +67,10 @@ window.customerAuth = {
     setPasswordUrl: @json(route('front.login.set-password')),
     registerUrl: '#',
     messages: {
-        notFound: @json(__('Bu nömrə ilə hesab tapılmadı. Qeydiyyatdan keçin.')),
-        otpSent: @json(__('OTP kod :mobile nömrəsinə göndərildi.')),
-        error: @json(__('Xəta baş verdi.')),
-        registration: @json(__('Qeydiyyat səhifəsini növbəti mərhələdə quracağıq.'))
+        notFound: @json(__('auth_no_account_found_for_this_number_please_register')),
+        otpSent: @json(__('auth_an_otp_was_sent_to_mobile')),
+        error: @json(__('auth_something_went_wrong')),
+        registration: @json(__('auth_registration_will_be_available_soon'))
     }
 };
 </script>
