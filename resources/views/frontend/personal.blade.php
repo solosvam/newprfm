d15a66f4f87f80ee0cd9524801f0a2890ecb4c34
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 03:47:12 +0400

    Translate storefront view text and form labels

--- resources/views/frontend/personal.blade.php
+++ resources/views/frontend/personal.blade.php
@@ -12,30 +12,30 @@
 @csrf
 <div class="form__non-credit">
     <div class="form__non-credit__container">
-        <input type="text" name="name" value="{{ old('name', auth()->user()->name) }}" placeholder="Adınız">
+        <input type="text" name="name" value="{{ old('name', auth()->user()->name) }}" placeholder="{{ __('Adınız') }}">
     </div>
     <div class="form__non-credit__container">
-        <input type="text" name="surname" value="{{ old('surname', auth()->user()->surname) }}" placeholder="Soyadınız">
+        <input type="text" name="surname" value="{{ old('surname', auth()->user()->surname) }}" placeholder="{{ __('Soyadınız') }}">
     </div>
     <div class="form__non-credit__container">
-        <input type="email" name="email" value="{{ old('email', auth()->user()->email) }}" placeholder="E-poçtunuz">
+        <input type="email" name="email" value="{{ old('email', auth()->user()->email) }}" placeholder="{{ __('E-poçtunuz') }}">
     </div>
     <div class="form__non-credit__container">
-        <input type="text" value="{{ auth()->user()->mobile }}" placeholder="Telefon nömrəniz" disabled>
+        <input type="text" value="{{ auth()->user()->mobile }}" placeholder="{{ __('Telefon nömrəniz') }}" disabled>
     </div>
     <div class="form__non-credit__container">
-        <input type="password" name="password" placeholder="Cari şifrəniz" autocomplete="current-password">
+        <input type="password" name="password" placeholder="{{ __('Cari şifrəniz') }}" autocomplete="current-password">
     </div>
     <div class="form__non-credit__container">
-        <input type="password" name="new_password" placeholder="Yeni şifrəniz" autocomplete="new-password">
+        <input type="password" name="new_password" placeholder="{{ __('Yeni şifrəniz') }}" autocomplete="new-password">
     </div>
     <div class="form__non-credit__container">
-        <input type="password" name="new_password_confirmation" placeholder="Yeni şifrə təkrar" autocomplete="new-password">
+        <input type="password" name="new_password_confirmation" placeholder="{{ __('Yeni şifrə təkrar') }}" autocomplete="new-password">
     </div>
 </div>
 
 @if($errors->any())<div class="profile-errors">{{ $errors->first() }}</div>@endif
-<div class="form__submit"><button type="submit">Məlumatları yenilə</button></div>
+<div class="form__submit"><button type="submit">{{ __('Məlumatları yenilə') }}</button></div>
 </form>
 </div>
 </div>
