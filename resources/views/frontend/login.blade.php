9384e025c222d809df0a703fb79ddfb56483e783
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 03:47:06 +0400

    Translate storefront view text and form labels

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -4,20 +4,20 @@
     <div class="login-container">
         <div class="login-page" id="loginBox">
             <div class="login-page__wrap">
-                <h1>Şəxsi kabinet</h1>
+                <h1>{{ __('Şəxsi kabinet') }}</h1>
                 <form class="login-page__wrap__form" id="customerLoginForm">
                     @csrf
                     <input type="tel" id="loginMobile" placeholder="994 __ ___ __ __" autocomplete="tel" inputmode="numeric" />
                     <div id="passwordArea" class="hide-form">
-                        <input type="password" id="loginPassword" placeholder="Şifrə" autocomplete="current-password" />
+                        <input type="password" id="loginPassword" placeholder="{{ __('Şifrə') }}" autocomplete="current-password" />
                     </div>
                     <p class="error-message hide-form" id="loginError"></p>
                     <div class="actions hide-form" id="passwordActions">
-                        <button type="submit" id="loginSubmitBtn">Daxil ol</button>
+                        <button type="submit" id="loginSubmitBtn">{{ __('Daxil ol') }}</button>
                     </div>
                 </form>
                 <div class="actions login-register-link">
-                    <button type="button" id="registerBtn">Qeydiyyat</button>
+                    <button type="button" id="registerBtn">{{ __('Qeydiyyat') }}</button>
                 </div>
             </div>
         </div>
@@ -25,18 +25,18 @@
         <div id="otpSection" class="hide-form">
             <div class="otpSection-wrapper">
                 <div class="headers">
-                    <h1>Hesabı təsdiqlə</h1>
+                    <h1>{{ __('Hesabı təsdiqlə') }}</h1>
                     <span class="otp-sent-message" id="otpMessage"></span>
-                    <input type="text" id="otpInput" inputmode="numeric" maxlength="6" placeholder="OTP kodu" />
+                    <input type="text" id="otpInput" inputmode="numeric" maxlength="6" placeholder="{{ __('OTP kodu') }}" />
                     <p class="error-message hide-form" id="otpError"></p>
                 </div>
                 <div class="otp-section-form-label">
                     <span class="seconds" id="otpTimer">01:00</span>
-                    <button type="button" class="send-again-text" id="resendOtp" disabled>Yenidən göndər</button>
+                    <button type="button" class="send-again-text" id="resendOtp" disabled>{{ __('Yenidən göndər') }}</button>
                 </div>
                 <div class="actions">
-                    <button type="button" id="backBtn">Geriyə</button>
-                    <button type="button" id="otpSubmitBtn">Təsdiqlə</button>
+                    <button type="button" id="backBtn">{{ __('Geriyə') }}</button>
+                    <button type="button" id="otpSubmitBtn">{{ __('Təsdiqlə') }}</button>
                 </div>
             </div>
         </div>
@@ -44,13 +44,13 @@
         <div id="setPasswordSection" class="hide-form">
             <div class="otpSection-wrapper">
                 <div class="headers">
-                    <h1>Şifrə təyin et</h1>
-                    <input type="password" id="newPassword" placeholder="Yeni şifrə" autocomplete="new-password" />
-                    <input type="password" id="newPasswordConfirmation" placeholder="Şifrənin təkrarı" autocomplete="new-password" />
+                    <h1>{{ __('Şifrə təyin et') }}</h1>
+                    <input type="password" id="newPassword" placeholder="{{ __('Yeni şifrə') }}" autocomplete="new-password" />
+                    <input type="password" id="newPasswordConfirmation" placeholder="{{ __('Şifrənin təkrarı') }}" autocomplete="new-password" />
                     <p class="error-message hide-form" id="passwordError"></p>
                 </div>
                 <div class="actions">
-                    <button type="button" id="setPasswordBtn">Şifrəni yadda saxla</button>
+                    <button type="button" id="setPasswordBtn">{{ __('Şifrəni yadda saxla') }}</button>
                 </div>
             </div>
         </div>
