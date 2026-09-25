6b2e0f33a698b1185489afe5d280972e036bb03a
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 03:47:09 +0400

    Translate storefront view text and form labels

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -7,33 +7,33 @@
             @include('frontend.partials.cabinet-sidebar')
             <div class="cabinet__sections">
                 <div class="cabinet__sections__elem bonus">
-                    <h2>Hesabınızdakı bonus pul</h2>
+                    <h2>{{ __('Hesabınızdakı bonus pul') }}</h2>
                     <p>0,00 AZN</p>
                     <a href="#">
                         <img src="{{ asset('frontend/images/arrow-bonus.svg') }}" alt="">
-                        <span>Bonus pul tarixçənizə baxın</span>
+                        <span>{{ __('Bonus pul tarixçənizə baxın') }}</span>
                     </a>
                 </div>
 
                 <div class="cabinet__sections__elem last-order">
-                    <h2>Son sifarişiniz</h2>
-                    <p>Hələ sifarişiniz yoxdur.</p>
+                    <h2>{{ __('Son sifarişiniz') }}</h2>
+                    <p>{{ __('Hələ sifarişiniz yoxdur.') }}</p>
                 </div>
 
                 <div class="cabinet__sections__elem order-location">
-                    <h2>Sifarişim haradadır?</h2>
-                    <p>Aktiv sifariş yoxdur.</p>
+                    <h2>{{ __('Sifarişim haradadır?') }}</h2>
+                    <p>{{ __('Aktiv sifariş yoxdur.') }}</p>
                 </div>
 
                 <div class="cabinet__sections__elem get-birkart">
                     <div class="info">
-                        <h3>BirKart əldə edin
+                        <h3>{{ __('BirKart əldə edin') }}
                             <svg width="22" height="9" viewBox="0 0 22 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M0.648804 4.5L20.6488 4.5" stroke="black" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
                                 <path d="M17.3931 1.24609L20.6489 4.50191L17.3931 7.75772" stroke="black" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
                         </h3>
-                        <span>Kreditlə sifarişdə çətinlik çəkirsinizsə, Birkart sifariş edin.</span>
+                        <span>{{ __('Kreditlə sifarişdə çətinlik çəkirsinizsə, Birkart sifariş edin.') }}</span>
                     </div>
                     <img src="{{ asset('frontend/images/birbank.png') }}" alt="">
                 </div>
@@ -41,8 +41,8 @@
                 <div class="cabinet__sections__elem invite-friend">
                     <ul>
                         <li>
-                            <h1>Dostunu dəvət et və bonus qazan!</h1>
-                            <button type="button" id="inviteButton">Dəvət göndər</button>
+                            <h1>{{ __('Dostunu dəvət et və bonus qazan!') }}</h1>
+                            <button type="button" id="inviteButton">{{ __('Dəvət göndər') }}</button>
                         </li>
                         <li><img src="{{ asset('frontend/images/invite.png') }}" alt=""></li>
                     </ul>
