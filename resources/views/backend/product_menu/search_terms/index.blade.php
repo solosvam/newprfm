1990fadf6d0e207f56436cda7939df44ee7fb86a
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 20:49:12 +0400

    Refresh visible search terms table script

--- resources/views/backend/product_menu/search_terms/index.blade.php
+++ resources/views/backend/product_menu/search_terms/index.blade.php
@@ -18,7 +18,7 @@
 @section('js_page')
     <script src="{{ asset('backend/js/vendor/datatables.min.js') }}"></script>
     <script src="{{ asset('backend/js/cs/datatable.extend.js') }}"></script>
-    <script src="{{ asset('backend/js/plugins/datatable.product-search-terms.ajax.js') }}?v=2"></script>
+    <script src="{{ asset('backend/js/plugins/datatable.product-search-terms.ajax.js') }}?v=3"></script>
 @endsection
 
 @section('content')
