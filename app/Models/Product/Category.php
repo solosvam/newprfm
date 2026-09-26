aacb04d15bd6ab04e2c6884155b51937c3043e7f
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 13:38:08 +0400

    Generate stable unique category slugs on creation

--- app/Models/Product/Category.php
+++ app/Models/Product/Category.php
@@ -4,6 +4,7 @@ namespace App\Models\Product;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Services\SeoUrl;
 
 class Category extends Model
 {
@@ -14,8 +15,21 @@ class Category extends Model
         'name_az',
         'name_en',
         'name_ru',
+        'slug',
         'active',
     ];
+
+    protected static function booted(): void
+    {
+        static::creating(function (self $category) {
+            if (!$category->slug) {
+                $category->slug = SeoUrl::uniqueDatabaseSlug(
+                    'categories',
+                    $category->name_az ?: $category->name_en ?: $category->name_ru ?: 'category'
+                );
+            }
+        });
+    }
 
     public function products()
     {
