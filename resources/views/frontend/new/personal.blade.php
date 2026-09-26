dca5089227cd43757abd431a169f969b40f44771
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 11:46:26 +0400

    Fix Blade parse error in personal form feedback

--- resources/views/frontend/new/personal.blade.php
+++ resources/views/frontend/new/personal.blade.php
@@ -16,17 +16,20 @@
                             <div class="form-field">
                                 <label>{{ __('profile_first_name') }}</label>
                                 <input id="personal-name" type="text" name="name" @class(['is-invalid' =>
-                                <div class="invalid-feedback" id="personal-name-error" data-error="name">{{ $errors->first('name') }}</div> $errors->has('name')]) aria-invalid="{{ $errors->has('name') ? 'true' : 'false' }}" aria-describedby="personal-name-error" value="{{ old('name', auth()->user()->name) }}" placeholder="{{ __('profile_first_name') }}">
+                                <div class="invalid-feedback" id="personal-name-error" data-error="name">{{ $errors->first('name') }}</div>
+                                 $errors->has('name')]) aria-invalid="{{ $errors->has('name') ? 'true' : 'false' }}" aria-describedby="personal-name-error" value="{{ old('name', auth()->user()->name) }}" placeholder="{{ __('profile_first_name') }}">
                             </div>
                             <div class="form-field">
                                 <label>{{ __('profile_last_name') }}</label>
                                 <input id="personal-surname" type="text" name="surname" @class(['is-invalid' =>
-                                <div class="invalid-feedback" id="personal-surname-error" data-error="surname">{{ $errors->first('surname') }}</div> $errors->has('surname')]) aria-invalid="{{ $errors->has('surname') ? 'true' : 'false' }}" aria-describedby="personal-surname-error" value="{{ old('surname', auth()->user()->surname) }}" placeholder="{{ __('profile_last_name') }}">
+                                <div class="invalid-feedback" id="personal-surname-error" data-error="surname">{{ $errors->first('surname') }}</div>
+                                 $errors->has('surname')]) aria-invalid="{{ $errors->has('surname') ? 'true' : 'false' }}" aria-describedby="personal-surname-error" value="{{ old('surname', auth()->user()->surname) }}" placeholder="{{ __('profile_last_name') }}">
                             </div>
                             <div class="form-field">
                                 <label>{{ __('profile_your_email') }}</label>
                                 <input id="personal-email" type="email" name="email" @class(['is-invalid' =>
-                                <div class="invalid-feedback" id="personal-email-error" data-error="email">{{ $errors->first('email') }}</div> $errors->has('email')]) aria-invalid="{{ $errors->has('email') ? 'true' : 'false' }}" aria-describedby="personal-email-error" value="{{ old('email', auth()->user()->email) }}" placeholder="{{ __('profile_your_email') }}">
+                                <div class="invalid-feedback" id="personal-email-error" data-error="email">{{ $errors->first('email') }}</div>
+                                 $errors->has('email')]) aria-invalid="{{ $errors->has('email') ? 'true' : 'false' }}" aria-describedby="personal-email-error" value="{{ old('email', auth()->user()->email) }}" placeholder="{{ __('profile_your_email') }}">
                             </div>
                             <div class="form-field">
                                 <label>{{ __('profile_your_phone_number') }}</label>
@@ -42,17 +45,20 @@
                             <div class="form-field">
                                 <label>{{ __('profile_current_password') }}</label>
                                 <input id="personal-password" type="password" name="password" @class(['is-invalid' =>
-                                <div class="invalid-feedback" id="personal-password-error" data-error="password">{{ $errors->first('password') }}</div> $errors->has('password')]) aria-invalid="{{ $errors->has('password') ? 'true' : 'false' }}" aria-describedby="personal-password-error" placeholder="{{ __('profile_current_password') }}" autocomplete="current-password">
+                                <div class="invalid-feedback" id="personal-password-error" data-error="password">{{ $errors->first('password') }}</div>
+                                 $errors->has('password')]) aria-invalid="{{ $errors->has('password') ? 'true' : 'false' }}" aria-describedby="personal-password-error" placeholder="{{ __('profile_current_password') }}" autocomplete="current-password">
                             </div>
                             <div class="form-field">
                                 <label>{{ __('profile_new_password') }}</label>
                                 <input id="personal-new_password" type="password" name="new_password" @class(['is-invalid' =>
-                                <div class="invalid-feedback" id="personal-new_password-error" data-error="new_password">{{ $errors->first('new_password') }}</div> $errors->has('new_password')]) aria-invalid="{{ $errors->has('new_password') ? 'true' : 'false' }}" aria-describedby="personal-new_password-error" placeholder="{{ __('profile_new_password') }}" autocomplete="new-password">
+                                <div class="invalid-feedback" id="personal-new_password-error" data-error="new_password">{{ $errors->first('new_password') }}</div>
+                                 $errors->has('new_password')]) aria-invalid="{{ $errors->has('new_password') ? 'true' : 'false' }}" aria-describedby="personal-new_password-error" placeholder="{{ __('profile_new_password') }}" autocomplete="new-password">
                             </div>
                             <div class="form-field">
                                 <label>{{ __('profile_repeat_new_password') }}</label>
                                 <input id="personal-new_password_confirmation" type="password" name="new_password_confirmation" @class(['is-invalid' =>
-                                <div class="invalid-feedback" id="personal-new_password_confirmation-error" data-error="new_password_confirmation">{{ $errors->first('new_password_confirmation') }}</div> $errors->has('new_password_confirmation')]) aria-invalid="{{ $errors->has('new_password_confirmation') ? 'true' : 'false' }}" aria-describedby="personal-new_password_confirmation-error" placeholder="{{ __('profile_repeat_new_password') }}" autocomplete="new-password">
+                                <div class="invalid-feedback" id="personal-new_password_confirmation-error" data-error="new_password_confirmation">{{ $errors->first('new_password_confirmation') }}</div>
+                                 $errors->has('new_password_confirmation')]) aria-invalid="{{ $errors->has('new_password_confirmation') ? 'true' : 'false' }}" aria-describedby="personal-new_password_confirmation-error" placeholder="{{ __('profile_repeat_new_password') }}" autocomplete="new-password">
                             </div>
                         </div>
 
