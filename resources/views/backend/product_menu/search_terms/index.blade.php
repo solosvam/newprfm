193c03bd944662c2797c7e85084ae83de7337468
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 21:03:43 +0400

    Load product list style search terms table

--- resources/views/backend/product_menu/search_terms/index.blade.php
+++ resources/views/backend/product_menu/search_terms/index.blade.php
@@ -18,7 +18,7 @@
 @section('js_page')
     <script src="{{ asset('backend/js/vendor/datatables.min.js') }}"></script>
     <script src="{{ asset('backend/js/cs/datatable.extend.js') }}"></script>
-    <script src="{{ asset('backend/js/plugins/datatable.product-search-terms.ajax.js') }}?v=4"></script>
+    <script src="{{ asset('backend/js/plugins/datatable.product-search-terms.ajax.js') }}?v=5"></script>
 @endsection
 
 @section('content')
