d85d102c7ed77b2095d561889ce0e2eb26d1eaf6
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 21:03:42 +0400

    Match product list DataTable for search terms

--- public/backend/js/plugins/datatable.product-search-terms.ajax.js
+++ public/backend/js/plugins/datatable.product-search-terms.ajax.js
@@ -2,36 +2,63 @@
  * Search term products DataTable
  */
 
-class ProductSearchTermsTable {
+class ProductSearchTermsAjax {
     constructor() {
         if (!jQuery().DataTable) {
+            console.log('DataTable is null!');
             return;
         }
 
+        this._datatable = null;
+        this._datatableExtend = null;
+        this._staticHeight = 62;
+
+        this._createInstance();
+        this._addListeners();
+        this._extend();
+    }
+
+    _createInstance() {
+        const _this = this;
+
         this._datatable = jQuery('#datatableProductSearchTerms').DataTable({
+            scrollX: true,
+            buttons: ['copy', 'excel', 'csv', 'print'],
             info: false,
             processing: true,
+
             ajax: {
-                url: '/admin/product/search-terms/products-data',
+                url: '/admin/product/list-data',
                 type: 'GET',
                 dataSrc: ''
             },
+
             order: [],
             sDom: '<"row"<"col-sm-12"<"table-container"t>r>><"row"<"col-12"p>>',
             pageLength: 10,
+
             columns: [
                 {data: null},
                 {data: 'brand'},
                 {data: 'name'},
                 {data: null}
             ],
+
             language: {
-                emptyTable: 'Məhsul tapılmadı.',
                 paginate: {
                     previous: '<i class="cs-chevron-left"></i>',
                     next: '<i class="cs-chevron-right"></i>'
                 }
             },
+
+            initComplete: function () {
+                _this._setInlineHeight();
+            },
+
+            drawCallback: function () {
+                _this._setInlineHeight();
+            },
+
             columnDefs: [
                 {
                     targets: 0,
@@ -45,16 +72,34 @@ class ProductSearchTermsTable {
                     targets: 3,
                     orderable: false,
                     searchable: false,
-                    className: 'text-end',
                     render: function (data, type, row) {
-                        return `<a class="btn btn-sm btn-outline-primary" href="/admin/product/search-terms/${row.id}">Aliasları idarə et</a>`;
+                        return `
+                            <a href="/admin/product/search-terms/${row.id}"
+                               class="btn btn-primary btn-sm product-search-terms-edit">
+                                Aliasları idarə et
+                            </a>
+                        `;
                     }
                 }
             ]
         });
+    }
 
-        new DatatableExtend({
+    _addListeners() {
+        const table = jQuery('#datatableProductSearchTerms');
+
+        table.on('click', '.product-search-terms-edit', function (e) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            window.location.href = this.href;
+        });
+    }
+
+    _extend() {
+        this._datatableExtend = new DatatableExtend({
             datatable: this._datatable,
+
             singleSelectCallback: function () {},
             multipleSelectCallback: function () {},
             anySelectCallback: function () {},
@@ -62,8 +107,22 @@ class ProductSearchTermsTable {
         });
     }
 
+    _setInlineHeight() {
+        if (!this._datatable) {
+            return;
+        }
+
+        const pageLength = this._datatable.page.len();
+        const scrollBody = document.querySelector(
+            '#datatableProductSearchTerms_wrapper .dataTables_scrollBody'
+        );
+
+        if (scrollBody) {
+            scrollBody.style.height = this._staticHeight * pageLength + 'px';
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    new ProductSearchTermsTable();
+    new ProductSearchTermsAjax();
 });
