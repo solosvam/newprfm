c97a93e273834a97bebd395472e155fe8868828e
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 20:36:56 +0400

    Add search term products DataTable

---
+++ public/backend/js/plugins/datatable.product-search-terms.ajax.js
@@ -0,0 +1,69 @@
+/**
+ * Search term products DataTable
+ */
+
+class ProductSearchTermsTable {
+    constructor() {
+        if (!jQuery().DataTable) {
+            return;
+        }
+
+        this._datatable = jQuery('#datatableProductSearchTerms').DataTable({
+            scrollX: true,
+            info: false,
+            processing: true,
+            ajax: {
+                url: '/admin/product/search-terms/products-data',
+                type: 'GET',
+                dataSrc: ''
+            },
+            order: [],
+            sDom: '<"row"<"col-sm-12"<"table-container"t>r>><"row"<"col-12"p>>',
+            pageLength: 10,
+            columns: [
+                {data: null},
+                {data: 'brand'},
+                {data: 'name'},
+                {data: null}
+            ],
+            language: {
+                emptyTable: 'Məhsul tapılmadı.',
+                paginate: {
+                    previous: '<i class="cs-chevron-left"></i>',
+                    next: '<i class="cs-chevron-right"></i>'
+                }
+            },
+            columnDefs: [
+                {
+                    targets: 0,
+                    orderable: false,
+                    searchable: false,
+                    render: function (data, type, row, meta) {
+                        return meta.row + meta.settings._iDisplayStart + 1;
+                    }
+                },
+                {
+                    targets: 3,
+                    orderable: false,
+                    searchable: false,
+                    className: 'text-end',
+                    render: function (data, type, row) {
+                        return `<a class="btn btn-sm btn-outline-primary" href="/admin/product/search-terms/${row.id}">Aliasları idarə et</a>`;
+                    }
+                }
+            ]
+        });
+
+        new DatatableExtend({
+            datatable: this._datatable,
+            singleSelectCallback: function () {},
+            multipleSelectCallback: function () {},
+            anySelectCallback: function () {},
+            noneSelectCallback: function () {}
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    new ProductSearchTermsTable();
+});
