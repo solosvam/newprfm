df54652659854ddb53fe31b50be89ea1f98cdec7
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 11:28:17 +0400

    Use legacy jQuery Notify for new frontend interactions

--- resources/views/frontend/new/partials/theme-script.blade.php
+++ resources/views/frontend/new/partials/theme-script.blade.php
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"></script>
 <script defer src="{{ asset('frontend/new/js/main.js') }}"></script>
 <script>
     document.getElementById('theme-toggle').addEventListener('click', function () {
