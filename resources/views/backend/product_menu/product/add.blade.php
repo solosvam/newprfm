6b0b53d42989cec52106435fac872fa3a930c628
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 13:47:14 +0400

    Align gender and product type side by side in product create form

--- resources/views/backend/product_menu/product/add.blade.php
+++ resources/views/backend/product_menu/product/add.blade.php
@@ -363,16 +363,33 @@
                                                     </select>
                                                 </div>
 
-                                                <div class="mt-3">
-                                                    <label for="gender">Cinsiyyət</label>
-                                                    <select class="form-select select-multiple" id="gender" multiple name="genders[]" required>
-                                                        @foreach($genders as $gender)
-                                                            <option value="{{ $gender->id }}" @selected(in_array($gender->id, old('genders', [])))>
-                                                                {{ $gender->name_az }}
-                                                            </option>
-                                                        @endforeach
-                                                    </select>
+                                                <div class="row mt-3">
+                                                    <div class="col-md-6">
+                                                        <div>
+                                                        <label for="gender">Cinsiyyət</label>
+                                                        <select class="form-select select-multiple" id="gender" multiple name="genders[]" required>
+                                                            @foreach($genders as $gender)
+                                                                <option value="{{ $gender->id }}" @selected(in_array($gender->id, old('genders', [])))>
+                                                                    {{ $gender->name_az }}
+                                                                </option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                    <div>
+                                                        <label for="type">Məhsul tipi</label>
+                                                        <select id="type" class="form-select select2" name="type_id" required>
+                                                            @foreach($types as $type)
+                                                                <option value="{{ $type->id }}" @selected(old('type_id') == $type->id)>
+                                                                    {{ $type->name_az }}
+                                                                </option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                    </div>
                                                 </div>
+
                                             </div>
 
                                             <div class="col-md-6">
@@ -387,17 +404,6 @@
                                                     @error('slug')<div class="invalid-feedback">{{ $message }}</div>@enderror
                                                     <small class="text-muted">Məsələn: amouage-opus-xv-king-blue</small>
                                                 </div>
-                                                <div class="mt-3">
-                                                    <label for="type">Məhsul tipi</label>
-                                                    <select id="type" class="form-select select2" name="type_id" required>
-                                                        @foreach($types as $type)
-                                                            <option value="{{ $type->id }}" @selected(old('type_id') == $type->id)>
-                                                                {{ $type->name_az }}
-                                                            </option>
-                                                        @endforeach
-                                                    </select>
-                                                </div>
-
                                                 <div class="mt-3">
                                                     <label for="ingredients">İnqrediyentlər</label>
                                                     <select class="form-select select2-tags" multiple name="ingredients[]" id="ingredients">
