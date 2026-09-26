6bb362a6b5d722f8e26c84ab72995b94a1824db1
Rufat Ibrahim <71541552+solosvam@users.noreply.github.com>
2026-09-26 20:50:56 +0400

    Refresh search terms table without scroll wrapper

--- resources/views/backend/product_menu/search_terms/index.blade.php
+++ resources/views/backend/product_menu/search_terms/index.blade.php
@@ -18,7 +18,7 @@
 @section('js_page')
     <script src="{{ asset('backend/js/vendor/datatables.min.js') }}"></script>
     <script src="{{ asset('backend/js/cs/datatable.extend.js') }}"></script>
-    <script src="{{ asset('backend/js/plugins/datatable.product-search-terms.ajax.js') }}?v=3"></script>
+    <script src="{{ asset('backend/js/plugins/datatable.product-search-terms.ajax.js') }}?v=4"></script>
 @endsection
 
 @section('content')
